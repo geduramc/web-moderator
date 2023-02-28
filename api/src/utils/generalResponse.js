cf79df3bb7fc2f3e5b7add376cbc5a3dc92da5bd
geduramc <geduradev@gmail.com>
2023-02-28 00:36:52 -0500

    chore: api add generalResponse json

---
+++ api/src/utils/generalResponse.js
@@ -0,0 +1,25 @@
+const ok = (_data) => {
+  return {
+    ok: true,
+    msg: 'Success',
+    data: _data
+  }
+}
+
+const info = (_msg, _data = null) => {
+  return {
+    ok: false,
+    msg: _msg,
+    data: (_data != null) ? _data : []
+  }
+}
+
+const error = (_msg) => {
+  return {
+    ok: false,
+    msg: _msg,
+    data: []
+  }
+}
+
+module.exports = { ok, info, error }
